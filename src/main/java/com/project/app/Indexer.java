98576f73fbc053a8ffd7d428517a3a4a921460e3
root <root@vml1wkxxx.cse.ust.hk>
2022-03-14 23:32:18 +0800

    Formatted properly, goes to text file

--- src/main/java/com/project/app/Indexer.java
+++ src/main/java/com/project/app/Indexer.java
@@ -9,6 +9,10 @@ Email:
 */
 
 import org.rocksdb.RocksDB;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import org.rocksdb.Options;
 import org.rocksdb.RocksDBException;
 import org.rocksdb.RocksIterator;
@@ -30,18 +34,18 @@ public class Indexer
     }
 
     //deprecated addEntry, new addEntry below
-    // public void addEntry(String word, int x, int y) throws RocksDBException
-    // {
-    //     // Add a "docX Y" entry for the key "word" into hashtable
-    //     // ADD YOUR CODES HERE
-    //     byte[] content = db.get(word.getBytes());
-    //     if (content == null) {
-    //         content = ("doc" + x + " " + y).getBytes();
-    //     } else {
-    //         content = (new String(content) + " doc" + x + " " + y).getBytes();
-    //     }
-    //     db.put(word.getBytes(), content);
-    // }
+    public void addEntry(String word, int x, int y) throws RocksDBException
+    {
+        // Add a "docX Y" entry for the key "word" into hashtable
+        // ADD YOUR CODES HERE
+        byte[] content = db.get(word.getBytes());
+        if (content == null) {
+            content = ("doc" + x + " " + y).getBytes();
+        } else {
+            content = (new String(content) + " doc" + x + " " + y).getBytes();
+        }
+        db.put(word.getBytes(), content);
+    }
 
     //just for phase one
     public void addEntry(String url, String data) throws RocksDBException
@@ -60,7 +64,8 @@ public class Indexer
         // Delete the word and its list from the hashtable
         // ADD YOUR CODES HERE
         db.delete(word.getBytes());
-    } 
+    }
+     
     public void printAll() throws RocksDBException
     {
         // Print all the data in the hashtable
@@ -68,43 +73,56 @@ public class Indexer
         RocksIterator iter = db.newIterator();
                     
         for(iter.seekToFirst(); iter.isValid(); iter.next()) {
-            System.out.println(new String(iter.key()) + "=" + new String(iter.value()));
+            System.out.println(new String(iter.value()));
+        }
+    }
+
+    public void toTextFile() throws RocksDBException
+    {
+        try(PrintWriter out = new PrintWriter("spider_result.txt")){
+            RocksIterator iter = db.newIterator();
+                        
+            for(iter.seekToFirst(); iter.isValid(); iter.next()) {
+                out.println(new String(iter.value()) + "\n------------------\n");
+            }
+        } catch (FileNotFoundException e){
+            System.err.println(e);
         }
     }
     
     public static void main(String[] args)
     {
-        // try
-        // {
-        //     // a static method that loads the RocksDB C++ library.
-        //     RocksDB.loadLibrary();
+        try
+        {
+            // a static method that loads the RocksDB C++ library.
+            RocksDB.loadLibrary();
 
-        //     // modify the path to your database
-        //     String path = "/root/comp4321project-g9-2022s/db";
+            // modify the path to your database
+            String path = "/root/comp4321project-g9-2022s/db";
             
-        //     // Indexer index = new Indexer(path);
+            Indexer index = new Indexer(path);
     
-        //     // index.addEntry("cat", 2, 6);
-        //     // index.addEntry("dog", 1, 33);
-        //     // System.out.println("First print");
-        //     // index.printAll();
+            index.addEntry("cat", 2, 6);
+            index.addEntry("dog", 1, 33);
+            System.out.println("First print");
+            index.printAll();
             
-        //     // index.addEntry("cat", 8, 3);
-        //     // index.addEntry("dog", 6, 73);
-        //     // index.addEntry("dog", 8, 83);
-        //     // index.addEntry("dog", 10, 5);
-        //     // index.addEntry("cat", 11, 106);
-        //     // System.out.println("Second print");
-        //     // index.printAll();
+            index.addEntry("cat", 8, 3);
+            index.addEntry("dog", 6, 73);
+            index.addEntry("dog", 8, 83);
+            index.addEntry("dog", 10, 5);
+            index.addEntry("cat", 11, 106);
+            System.out.println("Second print");
+            index.printAll();
             
-        //     // index.delEntry("dog");
-        //     // System.out.println("Third print");
-        //     // index.printAll();
-        // }
+            index.delEntry("dog");
+            System.out.println("Third print");
+            index.printAll();
+        }
         
-        // catch(RocksDBException e)
-        // {
-        //     System.err.println(e.toString());
-        // }
+        catch(RocksDBException e)
+        {
+            System.err.println(e.toString());
+        }
     }
 }
