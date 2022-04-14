a944072885e5a93ee623632c8dfd9279bfefedf3
root <root@vml1wkxxx.cse.ust.hk>
2022-04-14 15:01:07 +0800

    Changed some imports

--- src/main/java/com/project/app/StopStem.java
+++ src/main/java/com/project/app/StopStem.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Vector;
 
 public class StopStem {
 	private Porter porter;
