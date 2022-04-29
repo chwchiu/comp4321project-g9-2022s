2b5735f5122a3e983811859e95f9577a33cc98d7
root <root@vml1wkxxx.cse.ust.hk>
2022-04-29 19:23:14 +0800

    SearchEngine instructions

--- src/main/java/com/project/app/SearchEngine.java
+++ src/main/java/com/project/app/SearchEngine.java
@@ -3,10 +3,13 @@ package com.project.app;
 public class SearchEngine {
   String query;
 
+  //Constructor takes in a query
   public SearchEngine(String query){
     this.query = query;
   }
 
+  //Return a string thats the result of the search
+  //converts retrieval into string
   public String search(){
     return this.query;
   }
